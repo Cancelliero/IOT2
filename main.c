01fc7ac0293048455be4e1c1d01b0717c079ef80
Cancelliero <111314017+Cancelliero@users.noreply.github.com>
2023-10-18 11:48:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,29 +6,28 @@
 const uint TEMPERATURE_SENSOR_PIN = 26;
 
 void timer_callback(uint gpio, uint32_t events) {
-    uint32_t raw_value = adc_read();
-    float voltage = (float) raw_value * (3.3f / 65535.0f);
+    uint32_t raw = adc_read();
+    float voltage = (float) raw * (3.3f / 65535.0f);
     float temperature = (voltage - 0.5f) * 100.0f;
     printf("Temperatura do Nucleo: %.2f°C\n", temperature);
 }
 
-void setup() {
-    stdio_init_all();
-    
+
+int main(void) {
+     stdio_init_all();
+
     adc_init();
-    adc_gpio_init(TEMPERATURE_SENSOR_PIN);
+    adc_set_temp_sensor_enabled(TEMPERATURE_SENSOR_PIN);
     adc_select_input(0);
 
-    // Configure o timer para disparar a cada segundo
-    struct repeating_timer timer;
-    add_repeating_timer_ms(5000, timer_callback, NULL, &timer);
-}
-
-int main() {
-    setup();
-
-    while (1) {
-        tight_loop_contents();
+    while(1){
+        uint16_t raw = adc_read();
+        const float conversion = 3.3f / (1<<12);
+        float voltage =raw * conversion;
+        float temperature = 27 - (voltage - 0.706)/0.001721;
+        printf("Temperatura: %f °C\n", temperature);
+        sleep_ms(1000);
     }
+
     return 0;
 }
